8019ea9603209c4aac7043194caa42e535e0010a
hasitha <hasithakeshana9900@gmail.com>
2020-04-25 14:28:54 +0530

    add comments

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const routes = require('./routes/api');
 
-var cors = require('cors');
+const routes = require('./routes/api'); // email service route import from the API file 
+
+var cors = require('cors');// Cross-origin resource sharing (CORS) allows AJAX requests to skip the Same-origin policy and access resources from remote hosts.
 
 const app = express();
 
@@ -12,11 +13,15 @@ const uri = "mongodb+srv://rashmika:Rashmika@fashionstore-k14re.mongodb.net/test
 app.use(express.json());  //  useNewUrlParser: true, useFindAndModify: false
 
 app.use(express.urlencoded({extended:true}));
+/*
+Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option. This parser accepts only UTF-8 encoding of the body and supports automatic inflation of gzip and deflate encodings.
+A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body). This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true).
+*/
 
-app.use('/',routes);
+app.use('/',routes); // Use Api routes in the App
 
 
-app.listen(4003,function(){
+app.listen(4003,function(){ // Launch app to listen to specified port
 
     console.log('now listening for requests');
 });
--- routes/api.js
+++ routes/api.js
@@ -1,59 +1,79 @@
 const express = require('express');
+
 const router = express.Router();
-const nodemailer = require("nodemailer");
+
+const nodemailer = require("nodemailer"); // import nodemailer module for send  emails
 
 
+// create the endpoint(URL) for send emails for send alert messages when sensor levels up
 
-router.post('/sendEmail',async (req,res,next)=>{
+router.post('/sendEmail', async (req, res, next) => {
 
-    const receiverEmail = req.body.receiverEmail;
-    const senderMail = ''; // add sender email 
-    const password = ''; // add password
+  const receiverEmail = req.body.receiverEmail; // get the reciver email address from body of the  request
+  const senderMail = ''; // set emailmaddress of sender
+  const password = ''; // set password of sender
+
+
+/*
+    before send emails using node/express app need to allow low security features in google account
+    allow less secure feature on in google
+    link - https://myaccount.google.com/lesssecureapps
+*/
   
-    // allow less secure feature on in chrome
-    // link - https://myaccount.google.com/lesssecureapps
-  
-  try{
-  
-      
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    port: 25,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: senderMail, 
-      pass: password,
-    },
-    tls:{
-        rejectUnauthorized : false
-    }
-  });
-  let HelperOptions =  { from: senderMail, // sender address
-  to: receiverEmail, // list of receivers
-  subject: "Warning Message", // Subject line
-  text: "", // plain text body
-  html: `
-  <h3>Sensor Details</h3>
-  <li>location: ${req.body.location}</li>
-  <li>CO2 level: ${req.body.co2Level}</li>
-  <li>H20 level: ${req.body.h2oLevel}</li>
-  <h3>Message</h3>
-  <p>${req.body.message}</p>`
-  };
-  transporter.sendMail(HelperOptions,(error,info) =>{
-    if(error){
-        return console.log(error);
-    }
-    console.log("The message was sent!");
-    console.log(info);
-  
-    res.json(info);
-  });
-  
-  }
-  catch(e){
+try {
+
+      /*
+       create reusable transporter object using the default SMTP transport
+      */
+      let transporter = nodemailer.createTransport({ 
+          service: "gmail", // use gmail as the email service
+          port: 25, // port number
+          secure: false, // true for 465, false for other ports
+          auth: { // autnetication details
+              user: senderMail,
+              pass: password,
+          },
+          tls: {
+              rejectUnauthorized: false
+          }
+      });
+
+      let HelperOptions = {
+          from: senderMail, // sender address
+          to: receiverEmail, // list of receivers
+          subject: "Warning Message", // Subject line
+          text: "", // plain text body
+          html: ` 
+                  <h3>Sensor Details</h3>
+                  <li>location: ${req.body.location}</li>
+                  <li>CO2 level: ${req.body.co2Level}</li>
+                  <li>H20 level: ${req.body.h2oLevel}</li>
+                  <h3>Message</h3>
+                  <p>${req.body.message}</p>`
+        };
+
+      // HTML version of the message
+
+      transporter.sendMail(HelperOptions, (error, info) => { // send mail with defined transport object
+          if (error) {
+
+              return console.log(error); 
+
+          }
+
+          console.log("The message was sent!");
+
+          console.log(info);
+
+          res.json(info); // send the json response
+      });
+
+
+  } catch (e) {
       console.log(e);
   }
-  });
+
+
+});
 
 module.exports = router;
